9139609b0e4cb835c7c427ed0f4e639912e52fd9
iwans123 <bimasetyawan565@gmail.com>
2021-01-15 19:22:21 +0700

    upgrade about us 2

--- JS/biodata.js
+++ JS/biodata.js
@@ -1,14 +1,30 @@
 var SlideIndex=0;
+var SlideIndexTwo = 0;
 slideshow();
-
+slideshowTwo();
 function slideshow(){
     var i;
     var slide = document.getElementsByClassName("biodata");
+   
     for(var i = 0; i<slide.length; i++){
         slide[i].style.display = "none";
     }
+
     SlideIndex++;
     if(SlideIndex > slide.length){SlideIndex = 1}
     slide[SlideIndex-1].style.display = "block";
     setTimeout(slideshow, 2000);
+}
+function slideshowTwo(){
+    var j;
+
+    var slideTwo = document.getElementsByClassName("biodatatwo");
+    for(var j = 0; j<slideTwo.length; j++){
+        slideTwo[j].style.display = "none";
+    }
+
+    SlideIndexTwo++;
+    if(SlideIndexTwo > slideTwo.length){SlideIndexTwo = 1}
+    slideTwo[SlideIndexTwo-1].style.display = "block";
+    setTimeout(slideshowTwo, 2000);
 }
